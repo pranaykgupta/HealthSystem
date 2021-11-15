af9f296a063a9b9742fe7ead7d212386ed82243c
Pranay Kumar Gupta <pranaykgupta21@gmail.com>
2021-11-15 21:12:18 +0530

    update

--- src/js/app.js
+++ src/js/app.js
@@ -8,17 +8,17 @@ App = {
   },
 
   initWeb3: function() {
-    web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+    // web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
     // TODO: refactor conditional
-    // if (typeof web3 !== 'undefined') {
-    //   // If a web3 instance is already provided by Meta Mask.
-    //   App.web3Provider = web3.currentProvider;
-    //   web3 = new Web3(web3.currentProvider);
-    // } else {
-    //   // Specify default instance if no web3 instance provided
-    //   App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-    //   web3 = new Web3(App.web3Provider);
-    // }
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
     return App.initContract();
   },
 
